76dc4427b5db2dc78434a82cbf0da590dba3a2bf
Mohit <mikecoolexpert@gmail.com>
2025-09-24 13:42:02 +0530

    serverless Db connection

--- index.js
+++ index.js
@@ -58,18 +58,40 @@ app.use((req, res, next) => {
 });
 
 
-mongoose.connect(process.env.MONGO_URL).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((error) => {
-  console.error('Error connecting to MongoDB:', error);
-});
+// mongoose.connect(process.env.MONGO_URL).then(() => {
+//   console.log('Connected to MongoDB');
+// }).catch((error) => {
+//   console.error('Error connecting to MongoDB:', error);
+// });
 
 
-console.log(process.env.PORT)
+let isConnected = false;
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    isConnected = true;
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.log('Not Connected', err);
+  }
+}
 
-app.listen(process.env.PORT, () => {
-  console.log('Server suru ho gaya hai')
+
+app.use((req, res, next) => {
+  if (!isConnected) {
+    connectToMongoDB();
+  }
+  next();
 })
 
+// console.log(process.env.PORT)
 
-console.log('***********************')
+// app.listen(process.env.PORT, () => {
+//   console.log('Server suru ho gaya hai')
+// })
+
+
+// console.log('***********************')
+
+module.exports = app;
+
